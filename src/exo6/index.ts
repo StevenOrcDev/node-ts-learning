e4aec328a02df83e5eca7864b12b14c19a75da56
GABO <orcungabriel@gmail.com>
2025-05-27 20:37:55 +0200

    tout les exos

--- src/exo6/index.ts
+++ src/exo6/index.ts
@@ -4,4 +4,25 @@
 // - Supprimer un user
 // - Lister les users
 // - Chercher un user par nom
+import { readFileSync, writeFileSync } from 'fs'; 
 
+const chemin = './user.json';
+
+const fichier = readFileSync(chemin, "utf-8");
+let user = JSON.parse(fichier);
+
+// ajout 
+user.push({"name": "mika", "age": 41});
+// suprimer user
+user.splice(0, 1);
+// lister les users
+function liste() { for(let i=0; i < user.length; i++){console.log(user[i])}};
+// chercher par nom
+function cherche(nom: string): number{
+    for(let i =0;i<user.length;i++){
+        if(nom === user[i].name){
+            return i;
+        }
+    }
+    return -1;
+}
